7a11de01a5134974e6fa5c9e4515edbeeb7c4184
samid <sabarrera21@ucatolica.edu.co>
2025-08-28 15:50:57 -0500

    Footer estilos, estilos generales

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,25 @@
+
+import './Footer.css';
+
+const Footer = ({inScenario}) => {
+    console.log(inScenario);
+    return (
+        <footer className="game-footer">
+            <p className="footer-info">
+                🎓 Programa educativo de ciberseguridad
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
+// novatec solucionsoftware
+// novatec solucionsoftware
+// credibancoairflow
+// hibrido
+// metrolpolis 8-6pm 4
+// 70 prestaciones-30 no retencion
+//     * prueba formulario
+// * lider
+// * prueba tecnica
+// 80% para pasar
