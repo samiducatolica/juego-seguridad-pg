6f8418854b39a819e525e0144b0fcf8ee3a90000
samid <sabarrera21@ucatolica.edu.co>
2025-08-30 10:58:44 -0500

    crea componente Escenario y EscenarioStats

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,43 +1,64 @@
-import scenarios from '../../scenarios/menu/escenarios_list.json';
+import scenariosJson from "../../scenarios/menu/escenarios_list.json";
 import './Menu.css';
 
-const Menu = ({ onSelectScenario }) => {
+
+const EscenarioStats = ({stadistica, index}) => {
+    return (
+        <>
+            <div key={`${index}-tiempo`} className="stat-item">
+                <span>⏱️</span>
+                <span>{stadistica.tiempo}</span>
+            </div>
+            <div key={`${index}-tipo`} className="stat-item">
+                <span>🎯</span>
+                <span>{stadistica.tipo}</span>
+            </div>
+            <div key={`${index}-objetivo`} className="stat-item">
+                <span>📚</span>
+                <span>{stadistica.objetivo}</span>
+            </div>
+        </>
+    )
+}
+
+const Escenario = ({escenarioInfo, onSelectScenario}) => {
+    // console.log("Datos del escenario: ",escenarioInfo)
+    return (
+        <div key={escenarioInfo.id}
+             className={`scenario-card ${escenarioInfo.nombre}`}
+             onClick={
+                 () => onSelectScenario(escenarioInfo.nombre)
+             }>
+            <div className={"scenario-header"}>
+                <div className="scenario-icon">{escenarioInfo.icon}</div>
+                <div
+                    className={`difficulty-badge difficulty-${escenarioInfo.dificultad.toLowerCase()}`}>{escenarioInfo.dificultad}</div>
+            </div>
+            <h3 className="scenario-title">{escenarioInfo.titulo}</h3>
+            <p className="scenario-description" dangerouslySetInnerHTML={{__html: escenarioInfo.descripcion}}></p>
+            <div className="scenario-stats">
+                {escenarioInfo.estadisticas.map((stat, index) => (
+                        <EscenarioStats key={index} stadistica={stat} index={index}/>
+                    )
+                )}
+            </div>
+        </div>
+
+    )
+}
+
+const Menu = ({onSelectScenario}) => {
     return (
         <div className="main-content">
             <div className="scenarios-panel">
                 <h2 className="panel-title">🎮 Selecciona tu Misión de Seguridad</h2>
 
                 <div className="scenarios-grid">
-                    {scenarios.escenarios.map(scenario => (
-                        <div key={scenario.id} className={`scenario-card ${scenario.nombre}`}>
-                            <div className="scenario-header">
-                                <div className="scenario-icon">{scenario.icon}</div>
-                                <div className={`difficulty-badge difficulty-${scenario.dificultad.toLowerCase()}`}>{scenario.dificultad}</div>
-                            </div>
-                            <h3 className="scenario-title">{scenario.titulo}</h3>
-                            <p className="scenario-description" dangerouslySetInnerHTML={{ __html: scenario.descripcion }}></p>
-                            <div className="scenario-stats">
-                                {scenario.estadisticas.map((stat, index) => (
-                                    <>
-                                        <div key={`${index}-tiempo`} className="stat-item">
-                                            <span>⏱️</span>
-                                            <span>{stat.tiempo}</span>
-                                        </div>
-                                        <div key={`${index}-tipo`} className="stat-item">
-                                            <span>🎯</span>
-                                            <span>{stat.tipo}</span>
-                                        </div>
-                                        <div key={`${index}-objetivo`} className="stat-item">
-                                            <span>📚</span>
-                                            <span>{stat.objetivo}</span>
-                                        </div>
-                                    </>
-                                ))}
-                            </div>
-                            <button onClick={() => onSelectScenario(scenario.nombre)} className="scenario-btn">
-                                Jugar
-                            </button>
-                        </div>
+                    {scenariosJson.escenarios.map(juego => (
+                        <Escenario key={juego.id}
+                                   escenarioInfo={juego}
+                                   onSelectScenario={onSelectScenario}
+                        />
                     ))}
                 </div>
             </div>
